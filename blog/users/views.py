f552138a279eea7b30619ea53f6338c63a3b55be
Vitaliy <kulvitall.vk@gmail.com>
2023-05-25 15:15:38 +0300

    add bootstrap

--- blog/users/views.py
+++ blog/users/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
 
 users_app = Blueprint(
     name="users_app",
@@ -14,6 +15,15 @@ USERS = {
 }
 
 
-@users_app.route("/")
+@users_app.route("/", endpoint="list")
 def user_list():
     return render_template("users/list.html", users=USERS)
+
+
+@users_app.route("/<int:user_id>/", endpoint="details")
+def user_details(user_id: int):
+    try:
+        user_name = USERS[user_id]
+    except KeyError:
+        raise NotFound(f"User #{user_id} doesn't exist!")
+    return render_template("users/details.html", user_id=user_id, user_name=user_name)
