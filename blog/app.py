e3096e1e8e8c72bd2d3da3a45e8ec191e7d45e15
Vitaliy <kulvitall.vk@gmail.com>
2023-05-26 13:12:05 +0300

    init db, user model

--- blog/app.py
+++ blog/app.py
@@ -2,11 +2,16 @@ from flask import Flask, render_template
 
 from blog.users.views import users_app
 from blog.articles.views import articles_app
+from blog.models.database import db
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     register_blueprints(app)
+    app.config["SECRET_KEY"] = "!-3e@ck&icam8&-qc8fm+vk7mz=lj%)li@va+e7-7_@@qvdria"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
     @app.route("/")
     def index():
@@ -17,5 +22,8 @@ def create_app() -> Flask:
 def register_blueprints(app: Flask):
     app.register_blueprint(users_app)
     app.register_blueprint(articles_app)
+
+
+
 
 
---
+++ blog/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from blog.models.database import db
+
+
+class User(db.Model):
+    id = Column(Integer, primary_key=True)
+    username = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    is_staff = Column(Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f"User #{self.id} {self.username}"
