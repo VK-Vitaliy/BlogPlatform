b53271356b52bcc63065f0adc8a0d17858b86a2b
Vitaliy <kulvitall.vk@gmail.com>
2023-06-06 13:19:33 +0300

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,37 @@
-from flask_admin import Admin
+from flask import redirect, url_for
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user
+
 from blog import models
 from blog.models.database import db
 
 
+class MyAdminIndexView(AdminIndexView):
+    @expose("/")
+    def index(self):
+        if not (current_user.is_authenticated and current_user.is_staff):
+            return redirect(url_for("auth_app.login"))
+        return super(MyAdminIndexView, self).index()
+
+
 # Customized admin interface
 class CustomView(ModelView):
-    pass
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_staff
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for("auth_app.login"))
 
 
 # Create admin with custom base template
-admin = Admin(name="Blog Admin", template_mode="bootstrap4")
+admin = Admin(
+    name="Blog Admin",
+    index_view=MyAdminIndexView(),
+    template_mode="bootstrap4",
+)
 # Add views
-# admin.add_view(CustomView(models.Tag, db.session, category="Models"))
 admin.add_view(CustomView(models.Article, db.session, category="Models"))
 
 
